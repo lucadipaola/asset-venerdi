0cfc82419edd57c1b76f49485179a8e243b47828
lucadipa <luca-dipa-97@libero.it>
2023-02-06 17:23:55 +0100

    g2-esercitazione-java

---
+++ g2-esercitazione/g1/assets/js/index.js
@@ -0,0 +1,5 @@
+alert('Usa il pop up per dare dei messaggi');
+window.alert('Sto studiando JS');
+
+
+
---
+++ g2-esercitazione/g1/assets/js/naming.js
@@ -0,0 +1,22 @@
+//var 1 pet_preferito = 'gatto';
+//console.log(1 pet_preferito);
+
+/*var pet = 'gatto';
+var Pet = 'cane';*/
+var petPreferito = 'criceto';
+console.log(petPreferito);
+var PetPreferito = 'coniglio';
+console.log(PetPreferito);
+
+var $pet = 'giraffa';
+console.log($pet);
+var _pet = 'leone';
+console.log(_pet);
+var _pet2 = 'pantera';
+document.write(_pet2);
+
+var mariagrazio = 'topo'
+console.log(mariagrazio);
+mariagrazio = 'cavallo'
+console.log(mariagrazio);
+//var 12 = 'numero';
